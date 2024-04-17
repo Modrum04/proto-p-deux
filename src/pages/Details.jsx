aff532866bfcce5bfaa2ebab8e1ac7281303cb7d
Modrum <tristanlaroye@hotmail.com>
2024-04-17 20:31:08 +0200

    Ajout info details

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,6 +1,5 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import "./Details.scss";
 
 function Details() {
   let { filmid } = useParams();
@@ -15,6 +14,7 @@ function Details() {
   };
 
   useEffect(() => {
+    console.log(filmid);
     fetch(
       `https://api.themoviedb.org/3/movie/${filmid}?append_to_response=credits&language=fr`,
       options,
@@ -29,10 +29,49 @@ function Details() {
   return (
     <>
       <h1>{fetchResults.original_title}</h1>
+      {fetchResults.original_title !== fetchResults.title && <em>{fetchResults.title}</em>}
       <h2>Details</h2>
-      <p>
-        Release date : <em>{fetchResults.release_date?.split("-").reverse().join("-")}</em>
-      </p>
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${fetchResults.poster_path}`}
+        alt={`cover-${fetchResults.original_title}`}
+      />
+
+      <ul>
+        <li>
+          Genre :
+          {fetchResults.genres?.map((genre, i, arr) =>
+            i === arr.length - 1 ? genre.name : genre.name + ", ",
+          )}
+        </li>
+        <li>Durée : {fetchResults.runtime} minutes</li>
+        <li>
+          Version originale :{" "}
+          {fetchResults.spoken_languages?.map((language, i, arr) =>
+            i === arr.length - 1 ? language.iso_639_1 : language.iso_639_1 + ", ",
+          )}
+        </li>
+        <li>Release date : {fetchResults.release_date?.split("-").reverse().join("-")}</li>
+      </ul>
+      <ul>
+        <li>Note moyenne : {fetchResults.vote_average}/10</li>
+        <li>Nombre de votants : {fetchResults.vote_count}</li>
+        <li>Popularité : {fetchResults.popularity}</li>
+      </ul>
+      <ul>
+        <li>Réalisation : {fetchResults.credits?.crew[0].name}</li>
+        <li>
+          Société de production :{" "}
+          {fetchResults.production_companies?.map((companie, i, arr) =>
+            i === arr.length - 1 ? companie.name : companie.name + ", ",
+          )}
+        </li>
+        <li>
+          Pays d'origine :{" "}
+          {fetchResults.origin_country?.map((country, i, arr) =>
+            i === arr.length - 1 ? country : country + ", ",
+          )}
+        </li>
+      </ul>
       <h3>Crew details</h3>
       <div className="casting">
         {fetchResults.credits?.crew.map(
@@ -44,13 +83,12 @@ function Details() {
                     Job : <span>{crew.job}</span>
                   </p>
                   <p>
-                    Nom : <span>{crew.name}</span>{" "}
+                    Nom : <span>{crew.name}</span>
                   </p>
                 </div>
 
                 {crew.profile_path && (
                   <>
-                    <br />{" "}
                     <img
                       className="actor-img"
                       src={`https://image.tmdb.org/t/p/w500/${crew.profile_path}`}
